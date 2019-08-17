d29fc6ae2fec368a2652dce8094196cb75af784d
SRBarua <baruasudipta@gmail.com>
2019-08-17 10:59:46 -0600

    Playlist changes

--- app.js
+++ app.js
@@ -4,8 +4,20 @@ var app = new Vue ({
     //data as an object
     data: {
         songs: [
+            { title: 'Katy Perry', artist: 'Roar'},
             { title: 'Leroy Brown', artist: 'Jim Croce' },
             { title: 'Leroy Brown', artist: 'Jim Croce' },
+            { title: 'Celine Dion', artist: 'The Power of Love'},
+            { title: 'Lady Gaga', artist: 'Shallow'},
+            { title: 'Celine Dion', artist: 'I am Alive'},
+            { title: 'Celine Dion', artist: 'My Heart Will Go On'},
+            { title: 'Bryan Adams', artist: 'I Do It for You'},
+            { title: 'Bryan Adams', artist: 'Please Forgive Me'},
+            { title: 'Lionel Richie', artist: 'Hello'},
+            { title: 'Michael Jackson', artist: 'they Donot Care About Us'},
+            { title: 'Michael Jackson', artist: 'Black Or White'},
+            { title: 'Madonna', artist: 'La Isla Bonita'},
+            { title: 'Madonna', artist: 'Like a Prayer'}
         ],
         newSong: {
             title: '',
@@ -21,6 +33,10 @@ var app = new Vue ({
         },
         deleteSong(target) {
             this.songs.splice(target, 1)
+        },
+        toggleFavorite(target) {
+            this.songs[target].favorite = !this.songs[target].favorite;
+            console.log(this.songs[target].favorite);
         }
     }
 })
